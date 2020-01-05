3b730b9c231f142d8abcde40aa4401b2dfbb4560
Сергей <otto-vector@yandex.ru>
2020-01-05 16:57:25 +0300

    one cube in work done!

--- js/main.js
+++ js/main.js
@@ -1,104 +1,174 @@
 
-window.onload = init;
+window.onload = init
 
-var scene, camera, renderer, domEvents;
+var scene, camera, renderer, domEvents
 
-function init() {
-
-  scene = new THREE.Scene();
-  scene.background = new THREE.Color( "whitesmoke" );
-
-  camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
-  camera.position.set( 0, 0, 100 ); //позиция камеры
-  camera.lookAt( 0, 0, 0 );
+function init() { 
   
-  renderer = new THREE.WebGLRenderer();
-  renderer.setSize( window.innerWidth, window.innerHeight );
+  //добавил сцену
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color( "whitesmoke" )
+  
+  //настроил параметры камеры
+  camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 )
+  camera.position.set( 0, 0, 75 ) //позиция камеры
+  camera.lookAt( 0, 0, 0 )
+  
+  //выбрал рендер
+  renderer = new THREE.WebGLRenderer()
+  renderer.setPixelRatio(window.devicePixelRatio)
+  renderer.setSize( window.innerWidth-4, window.innerHeight-4 )
 
   //добавление скрипта к документу в тег
-  document.querySelector("body").appendChild( renderer.domElement );
+  document.querySelector("body").appendChild( renderer.domElement )
   
-  //функция для создания куба из линий vector3
-  function one_cube( numb_x = 10, numb_y = numb_x, numb_z = numb_x * 1.5 ) {
-    var geometry_n = new THREE.Geometry();
-    var cube_array = [ [-1,0,0], [0,1,0], [1,0,0], [0,-1,0], [-1,0,0], [-1,0,1],
-      [0,1,1], [1,0,1], [0,-1,1], [-1,0,1], [-1,0,0], [0,1,0], [0,1,1], [1,0,1],
-      [1,0,0], [0,-1,0], [0,-1,1] ];
-    for (var i = 0; i < cube_array.length; i++)
-      geometry_n.vertices.push( new THREE.Vector3( numb_x*cube_array[i][0], numb_y*cube_array[i][1], numb_z*cube_array[i][2]) );
+  window.addEventListener('resize', onWindowResize, false)
 
-    return geometry_n;
-  }
+  const points_array = [ [-1,0,0], [0,1,0], [1,0,0], [0,-1,0], //ближний квадрат
+                         [-1,0,1], [0,1,1], [1,0,1], [0,-1,1] ] //дальний квадрат
+  
+  const communication_array = [ [0,1], [1,2], [2,3], [3,0], //связь рёбер основания куба
+                                [4,5], [5,6], [6,7], [7,4], //связь рёбер верхнего квадрата куба
+                                [0,4], [1,5], [2,6], [3,7] ] // связь рёбер между верхним и нижним квадратом куба
 
-  function sphere_xyz(numb_x = 10, numb_y = numb_x, numb_z = numb_x * 1.5) {
-    var sphere_array = [ [-1,0,0], [0,1,0], [1,0,0], [0,-1,0], [-1,0,1],
-    [0,1,1], [1,0,1], [0,-1,1] ];
-    var geo = new THREE.SphereGeometry( 1, 32, 32 );
-    var sphere = [];
-    for (var i = 0; i < sphere_array.length; i++) {
-      sphere[i] = new THREE.Mesh( geo );
-      sphere[i].position.set( numb_x*sphere_array[i][0], numb_y*sphere_array[i][1], numb_z*sphere_array[i][2]);
-      sphere[i].material.color.set( Math.random() * 0xffffff );
+  const cube_color = 0x000000 //цвет рёбер куба по умолчанию
+
+  //Линии для куба
+  function lines_for_cube(numb_x = 10, numb_y = numb_x, numb_z = numb_x * 1.5) {
+
+    var geometry_for_line = []
+    
+    for (let i = 0 ; i < communication_array.length ; i++ ) {
+
+      geometry_for_line[i] = new THREE.Geometry()
+      
+      let x_start = numb_x*points_array[communication_array[i][0]][0]
+      let y_start = numb_y*points_array[communication_array[i][0]][1]
+      let z_start = numb_z*points_array[communication_array[i][0]][2]
+      
+      geometry_for_line[i].vertices.push( new THREE.Vector3( x_start, y_start, z_start ) )
+
+      let x_finish = numb_x*points_array[communication_array[i][1]][0]
+      let y_finish = numb_y*points_array[communication_array[i][1]][1]
+      let z_finish = numb_z*points_array[communication_array[i][1]][2]
+      
+      geometry_for_line[i].vertices.push( new THREE.Vector3( x_finish, y_finish, z_finish ) )
+
     }
-    return sphere;
+
+    return geometry_for_line
+
   }
 
+  ////////функция сборки сфер по углам куба////////////////
+  function sphere_xyz(numb_x = 10, numb_y = numb_x, numb_z = numb_x * 1.5) {
+
+    var geo = new THREE.SphereGeometry( 1, 32, 32 )
+    var sphere = []
+
+    for (var i = 0; i < points_array.length; i++) {
+    
+      let x = numb_x*points_array[i][0]
+      let y = numb_y*points_array[i][1]
+      let z = numb_z*points_array[i][2]
+
+      sphere[i] = new THREE.Mesh( geo )
+      sphere[i].position.set( x, y, z)
+      sphere[i].material.color.set( Math.random() * 0xffffff ) //задание произвольного цвета сфер
+
+    }
+
+    return sphere
+  }
   
-  ////////////////////////////////////////////////////
-  var line_set = new MeshLine();
-  var line2_set = new MeshLine();
-  line_set.setGeometry( one_cube(), function( p ) { return 0.5 } ); //return и есть ширина линии
-  line2_set.setGeometry( one_cube( 8 ), function( p ) { return 0.8 } );
+  //Cоздание куба из линий массива в группу
+  var geometry_for_line = lines_for_cube(10)
+  var edge_cube = new THREE.Group()
+  var line_set = edge = []
 
-  var line = new THREE.Mesh( line_set.geometry, new MeshLineMaterial() );
-  line.material.color.set( "green" );
-  var line2 = new THREE.Mesh( line2_set.geometry, new MeshLineMaterial() );
-  line2.position.set( 25, 25, 0 );
-  line2.material.color.set( "red" );
+  for (let i = 0; i < communication_array.length; i++) {
+    //поменял размер линий с помощью библиотеки THREE.MeshLine.js
+    line_set[i] = new MeshLine()
+    line_set[i].setGeometry( geometry_for_line[i], function( p ) { return 0.5 } ) //return и есть ширина линии
 
-  // scene.add( line );
-  scene.add( line2 );
+    edge[i] = new THREE.Mesh( line_set[i].geometry, new MeshLineMaterial() )
+    edge[i].material.color.set( cube_color )
 
-  //////////////////////////////////////////////
-  var spheres = sphere_xyz();
-  // for (let i=0; i < spheres.length; i++) scene.add(spheres[i]);
+    edge_cube.add( edge[i] )
 
-  var group = new THREE.Group();
-  group.add( line );
-  for (let i=0; i < spheres.length; i++) group.add( spheres[i] );
-  scene.add( group );
+  }
 
+  ///Cоздание сфер в куб на 10 и назначение их в массив////////////////////////
+  var spheres = sphere_xyz(10)
+  
+  ////группировка объектов в куб со сферами/////////////////////
+  var spheres_in_corner = new THREE.Group()
+
+  for (let i=0; i < spheres.length; i++) spheres_in_corner.add( spheres[i] )
+
+  var cube_with_spheres_in_corner = new THREE.Group()
+  cube_with_spheres_in_corner.add(edge_cube)
+  cube_with_spheres_in_corner.add(spheres_in_corner)
+
+  /////добавление объектов на экран//////////
+  scene.add( cube_with_spheres_in_corner )
+
+  ////анимация объектов////////////////////
   animate()
 
-    function animate() {
-    // note: three.js includes requestAnimationFrame shim
+  function animate() {
+  
     requestAnimationFrame( animate )
     render()
-    }
-
-    function render() {
-    //задание значений для поворота объекта
-    function rotation(name_of_object, x,y) {
-    name_of_object.rotation.x += x; name_of_object.rotation.y += y;
-    }
-    rotation(line2,0.01,0.02)
-    rotation(group,0.02,0.01)
-
-    renderer.render( scene, camera )
-    }
-
-  document.querySelector( "body" ).onclick = function () {
-    // line.material.color.set( "black" );
-    // spheres[1].material.color.set( 0xffff00 )
-    // scene.background = new THREE.Color( Math.random() * 0x00ff00 );
+  
   }
 
+  function render() {
   
-  domEvents = new THREEx.DomEvents(camera, renderer.domElement)
+    //задание значений для поворота объекта
+    function rotation( name_of_object, x, y ) {
+    
+      name_of_object.rotation.x += x/100
+      name_of_object.rotation.y += y/100
+    
+    }
+    
+    rotation( cube_with_spheres_in_corner, 2, 1 )
 
-  domEvents.addEventListener( spheres[1], 'click', event => {
-    console.log('here!')
-    spheres[1].material.color.set( 0xff0000 )
-  })
+    renderer.render( scene, camera )
+  
+  }
+
+
+  ///////// применил отслеживание по клику с помощью библиотеки threex.domevents.js ////////
+  var domEvents = new THREEx.DomEvents(camera, renderer.domElement)
+  
+  for (sphere_num = 0; sphere_num < spheres.length; sphere_num++) {
+    domEvents.addEventListener( spheres[sphere_num], 'click', (event) => {
+      
+      var num = 0;
+      //проверяет на какую из сфер было нажато и передаёт значение индекса в num
+      for (let i = 0; i < spheres.length; i++ ) {
+        num = (event.target.uuid == spheres[i].uuid) ? i : num;
+      }
+
+      //перекраска рёбер, исходящих из нажатой сферы в её цвет
+      for (let i = 0; i < communication_array.length; i++) {
+          if (communication_array[i][0] == num || communication_array[i][1] == num )
+          edge[i].material.color.set(spheres[num].material.color)
+      }
+
+    })
+  }
+
 
 }
+
+  function onWindowResize() {
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth-4, window.innerHeight-4);
+    
+  }
