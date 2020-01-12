829ff7c86909c71425163bd1c138de7f9f04f085
otto-vector <otto-vector@yandex.ru>
2020-01-13 01:17:41 +0300

    add params to object test and some changes

--- js/main.js
+++ js/main.js
@@ -4,11 +4,12 @@ window.onload = init
 var scene, camera, renderer, domEvents
 
 function init() {
+
   //////////Задал основные константы///////
   const cubes_maximum = Math.floor(Math.random() * 15) + 2 //произвольное количество кубов, но не меньше 2
-  const range_of_positions = 100 // разлёт появления кубов (квадрат с серединой в центре)
+  const range_of_positions = 100 // разлёт появления кубов в пикселях (квадрат с серединой в центре)
   const cube_color = 0x000000 //цвет рёбер куба по умолчанию
-  const rotation_speed = 250; //чем выше значение, тем медленнее
+  const rotation_speed = 5; //значение скорости вращения для (опция)
 
   var edge = [] //двумерный массив для рёбер кубов
   var spheres = [] //двумерный массив для сфер кубов
@@ -16,8 +17,8 @@ function init() {
 
   const points_array = [ [-1,0,0], [0,1,0], [1,0,0], [0,-1,0], //ближний квадрат
                          [-1,0,1], [0,1,1], [1,0,1], [0,-1,1] ] //дальний квадрат
-  
-  const communication_array = [ [0,1], [1,2], [2,3], [3,0], //связь рёбер основания куба
+
+  const communication_array = [ [0,1], [1,2], [2,3], [3,0], //связь рёбер основания куба // по номеру записи points_array
                                 [4,5], [5,6], [6,7], [7,4], //связь рёбер верхнего квадрата куба
                                 [0,4], [1,5], [2,6], [3,7] ] // связь рёбер между верхним и нижним квадратом куба
 
@@ -25,12 +26,12 @@ function init() {
   //добавил сцену
   scene = new THREE.Scene();
   scene.background = new THREE.Color( "whitesmoke" ) //задал сцене задний фон
-  
+
   //настроил параметры камеры
   camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 )
   camera.position.set( 0, 0, 150 ) //позиция камеры
   camera.lookAt( 0, 0, 0 ) //смотреть в центр координат
-  
+
   //выбрал рендер
   renderer = new THREE.WebGLRenderer()
   renderer.setPixelRatio(window.devicePixelRatio)
@@ -42,51 +43,55 @@ function init() {
   window.addEventListener('resize', onWindowResize, false)
 
 
-///////////////////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////////////
   //конструктор линий для куба
-  function lines_for_cube(numb_x = 10, numb_y = numb_x, numb_z = numb_x * 1.5) {
+  var lines_for_cube = function (numb_x = 10, numb_y = numb_x, numb_z = numb_x * 1.5) {
 
     let geometry_for_line = edge_in_function = []
     let x_start, y_start, z_start //задание переменных для начальных координат линии вектора
     let x_finish, y_finish, z_finish //задание переменных для финишных координат линии вектора
-    
+
     for (let i = 0 ; i < communication_array.length ; i++ ) {
 
-      geometry_for_line[i] = new THREE.Geometry()
+      geometry_for_line[i] = new THREE.Geometry();
       
-      x_start = numb_x * points_array [ communication_array[i][0] ] [0]
-      y_start = numb_y * points_array [ communication_array[i][0] ] [1]
-      z_start = numb_z * points_array [ communication_array[i][0] ] [2]
+      x_start = numb_x * points_array [ communication_array[i][0] ] [0];
+      y_start = numb_y * points_array [ communication_array[i][0] ] [1];
+      z_start = numb_z * points_array [ communication_array[i][0] ] [2];
       
-      geometry_for_line[i].vertices.push( new THREE.Vector3( x_start, y_start, z_start ) )
+      geometry_for_line[i].vertices.push( new THREE.Vector3( x_start, y_start, z_start ) );
 
-      x_finish = numb_x * points_array [ communication_array[i][1] ] [0]
-      y_finish = numb_y * points_array [ communication_array[i][1] ] [1]
-      z_finish = numb_z * points_array [ communication_array[i][1] ] [2]
+      x_finish = numb_x * points_array [ communication_array[i][1] ] [0];
+      y_finish = numb_y * points_array [ communication_array[i][1] ] [1];
+      z_finish = numb_z * points_array [ communication_array[i][1] ] [2];
       
-      geometry_for_line[i].vertices.push( new THREE.Vector3( x_finish, y_finish, z_finish ) )
+      geometry_for_line[i].vertices.push( new THREE.Vector3( x_finish, y_finish, z_finish ) );
 
 
-      edge_in_function[i] = ( new THREE.Line(
+      edge_in_function[i] = (
+                              new THREE.Line(
                                 geometry_for_line[i],
                                 new THREE.LineBasicMaterial( { color: 0x000000 } )
                                 )
                             );
 
+      // edge_in_function[i].ex_position = communication_array[i] //добавление параметра в объект
+
     }
 
-    return edge_in_function
+    return edge_in_function;
 
   }
 
   ////////функция сборки сфер по углам куба////////////////
-  function spheres_for_cube(numb_x = 10, numb_y = numb_x, numb_z = numb_x * 1.5) {
+
+   var spheres_for_cube = function(numb_x = 10, numb_y = numb_x, numb_z = numb_x * 1.5) {
 
     let geo = new THREE.SphereGeometry( numb_x * 0.13, 32, 32 ) //меняет размер в зависимости от величины квадрата
     let sphere = []
 
     for (var i = 0; i < points_array.length; i++) {
-    
+
       let x = numb_x * points_array[i][0]
       let y = numb_y * points_array[i][1]
       let z = numb_z * points_array[i][2]
@@ -103,7 +108,7 @@ function init() {
   //////////////////////////////////////////////////////////////////////
 
   //Cоздание куба из линий массива в группу///
-  function complete_to_object(next) {
+  var complete_to_object = function (next) {
 
     //объявление переменных
     let edge_cube = new THREE.Group()
@@ -152,16 +157,16 @@ function init() {
     //задание значений для поворота объекта
     function rotation( name_of_object, x, y, speed ) {
     
-      name_of_object.rotation.x += x/speed
-      name_of_object.rotation.y += y/speed
+      name_of_object.rotation.x += x/1000*speed
+      name_of_object.rotation.y += y/1000*speed
     
     }
 
     for (let i=0; i < cubes_maximum; i++) //нечетные крутятся в другую сторону
       rotation( cube_with_spheres_in_corner[i],
-      			(i%2)? 2 : -1, //x-rotation
-      			(i%2)? 1 : -2, //y-rotation
-      			rotation_speed )
+        (i%2)? 2 : -1, //x-rotation
+        (i%2)? 1 : -2, //y-rotation
+        rotation_speed )
 
     renderer.render( scene, camera )
   
@@ -170,11 +175,12 @@ function init() {
 
   ///////// применил отслеживание по клику с помощью библиотеки threex.domevents.js ////////
   var domEvents = new THREEx.DomEvents(camera, renderer.domElement)
-  
+
   for (let n = 0; n < cubes_maximum; n++) {
     for (let sphere_num = 0; sphere_num < spheres[n].length; sphere_num++) {
 
       domEvents.addEventListener( spheres[n][sphere_num], 'click', (event) => {
+
         for (let n = 0; n < cubes_maximum; n++) {
           //проверяет на какую из сфер было нажато и передаёт значение индекса в found_at
           var found_at = [];
